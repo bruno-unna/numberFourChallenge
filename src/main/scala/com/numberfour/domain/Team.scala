28aced98e553423ee869168342c87e622ad81428
Bruno Unna <bruno.unna@gmail.com>
2013-10-01 20:01:12 +0200

    attempt to use a better autoincremental

--- src/main/scala/com/numberfour/domain/Team.scala
+++ src/main/scala/com/numberfour/domain/Team.scala
@@ -12,24 +12,26 @@ case class SubTeam(name: String)
 case object TeamManager {
 
   // persistence related stuff:
-  def getMongoCollection() = {
-    val mongoClient = MongoClient("localhost", 27017)
-    val db = mongoClient("numberfour")
-    db("team")
-  }
-  val coll = getMongoCollection
+  val mongoClient = MongoClient("localhost", 27017)
+  val db = mongoClient("numberfour")
+  val teamCollection = db("team")
+//  def nextId(collection: String): Int = {
+//    val counterCollection = db("counters")
+//    val counterSelector = MongoDBObject("_id" -> collection)
+//    val counter = counterCollection.findOne(counterSelector)
+//  }
 
   def create(name: String): Team = {
     val selector = MongoDBObject("name" -> name)
-    val readTeam = coll.findOne(selector)
+    val readTeam = teamCollection.findOne(selector)
     val team: Team = if (readTeam.isDefined) {
       val t = readTeam.get
       Team(t.getAs[Int]("id").get, t.getAs[String]("name").get, t.getAs[Int]("members").get)
     } else {
       // TODO find a better way to generate ids
-      val nextId = coll.count().toInt + 1
-      val insertable = MongoDBObject("id" -> nextId, "name" -> name, "members" -> 0)
-      coll.insert(insertable)
+      val nextId = teamCollection.count().toInt + 1
+      val insertable = MongoDBObject("_id" -> "getNextSequence(\"team\")", "name" -> name, "members" -> 0)
+      teamCollection.insert(insertable)
       // TODO validate correct db write
 
       Team(nextId, name, 0)
@@ -39,7 +41,7 @@ case object TeamManager {
 
   def findById(id: Int): Option[Team] = {
     val selector = MongoDBObject("id" -> id)
-    val readTeam = coll.findOne(selector)
+    val readTeam = teamCollection.findOne(selector)
     if (readTeam.isEmpty) None
     else {
       val t = readTeam.get
